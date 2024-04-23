d64107e499c2d55040494c832904cdb3d95fd6ed
ureonmyhitlist <ureonmyhitlist@naver.com>
2024-04-23 11:25:36 +0900

    edit : styled component 도입

--- allclear/src/App.jsx
+++ allclear/src/App.jsx
@@ -1,37 +1,57 @@
 import { useState } from "react";
-import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import Monitoring from "./components/Monitoring.jsx";
 import Statistics from "./components/Statistics.jsx";
 import { RecoilRoot } from "recoil";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
   return (
     <>
       <RecoilRoot>
         <BrowserRouter>
-          <div className="container">
+          <Container>
             <Navbar />
-            <div className="main-container">
-              <div className="sidebar-container">
+            <MainContainer>
+              <SidebarContainer>
                 <Sidebar />
-              </div>
-              <div className="content-container">
+              </SidebarContainer>
+              <ContentContainer>
                 <Routes>
                   <Route path="/dashboard" element={<Dashboard />}></Route>
                   <Route path="/monitoring" element={<Monitoring />}></Route>
                   <Route path="/statistics" element={<Statistics />}></Route>
                 </Routes>
-              </div>
-            </div>
-          </div>
+              </ContentContainer>
+            </MainContainer>
+          </Container>
         </BrowserRouter>
       </RecoilRoot>
     </>
   );
 }
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const MainContainer = styled.div`
+  display: flex;
+  height: calc(100% - 80px);
+`;
+
+const SidebarContainer = styled.div`
+  width: 18%;
+  height: 100%;
+  border-right: 1.5px solid #324254;
+`;
+
+const ContentContainer = styled.div`
+  width: 82%;
+  border-left: 1.5px solid #324254;
+`;
 export default App;
