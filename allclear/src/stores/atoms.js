952a5334c6c3f44af5204470bad1b359201b3d6d
ureonmyhitlist <ureonmyhitlist@naver.com>
2024-04-23 16:06:29 +0900

    edit : recoil persist 수정

--- allclear/src/stores/atoms.js
+++ allclear/src/stores/atoms.js
@@ -1,6 +1,13 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "persist-atom-key",
+  storage: sessionStorage,
+});
 
 export const isLoggedInAtom = atom({
   key: "isLoggedIn",
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
