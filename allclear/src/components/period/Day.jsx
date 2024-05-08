5b2e6a4308b5f445ace926ecf2b3b04d822e111c
ureonmyhitlist <ureonmyhitlist@naver.com>
2024-05-08 16:35:36 +0900

    edit : 일간, 주간 데이터 연동

--- allclear/src/components/period/Day.jsx
+++ allclear/src/components/period/Day.jsx
@@ -1,28 +1,22 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { getHourlyData } from "../../apis/statistic/statisticData";
 import { axisClasses } from "@mui/x-charts/ChartsAxis";
 
 function Day() {
-  const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-  const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-  const xLabels = [
-    "Page A",
-    "Page B",
-    "Page C",
-    "Page D",
-    "Page E",
-    "Page F",
-    "Page G",
-  ];
-  const [hourlyData, setHourlyData] = useState({});
+  const [hourlyTemperature, setHourlyTemperature] = useState([]);
+  const [hourlyHumidity, setHourlyHumidity] = useState([]);
+  const [hourlyLight, setHourlyLight] = useState([]);
+  const [hourlyCheckAt, setHourlyCheckAt] = useState([]);
 
   useEffect(() => {
     getHourlyData(
       ({ data }) => {
-        setHourlyData(data);
-        console.log(hourlyData);
+        setHourlyTemperature(data.temperatureList);
+        setHourlyHumidity(data.humidityList);
+        setHourlyLight(data.lightList);
+        setHourlyCheckAt(data.checkAtList);
       },
       (error) => {
         console.log(error);
@@ -36,17 +30,77 @@ function Day() {
         <LineChart
           width={700}
           height={400}
+          colors={["#E03F69"]}
           series={[
-            { data: pData, label: "pv" },
-            { data: uData, label: "uv" },
+            { data: hourlyTemperature, label: "온도" },
+            // { data: hourlyHumidity, label: "습도" },
+            // { data: hourlyLight, label: "조도" },
           ]}
-          xAxis={[{ scaleType: "point", data: xLabels }]}
-          // series={[
-          //   { data: hourlyData.temperature, label: "온도" },
-          //   { data: hourlyData.humidity, label: "습도" },
-          //   { data: hourlyData.light, label: "조도" },
-          // ]}
-          // xAxis={[{ scaleType: "point", data: hourlyData.checkAt }]}
+          xAxis={[{ scaleType: "point", data: hourlyCheckAt }]}
+          sx={{
+            [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 축 라벨의 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // x축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // y축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // x축 눈금의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // y축 눈금의 색상을 흰색으로 설정
+            },
+          }}
+          slotProps={{ legend: { labelStyle: { fill: `#e6e5ea` } } }}
+        />
+        <LineChart
+          width={700}
+          height={400}
+          colors={["#4A5ED8"]}
+          series={[
+            // { data: hourlyTemperature, label: "온도" },
+            { data: hourlyHumidity, label: "습도" },
+            // { data: hourlyLight, label: "조도" },
+          ]}
+          xAxis={[{ scaleType: "point", data: hourlyCheckAt }]}
+          sx={{
+            [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 축 라벨의 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // x축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // y축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // x축 눈금의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // y축 눈금의 색상을 흰색으로 설정
+            },
+          }}
+          slotProps={{ legend: { labelStyle: { fill: `#e6e5ea` } } }}
+        />
+        <LineChart
+          width={700}
+          height={400}
+          colors={["#F6C863"]}
+          series={[
+            // { data: hourlyTemperature, label: "온도" },
+            // { data: hourlyHumidity, label: "습도" },
+            { data: hourlyLight, label: "조도" },
+          ]}
+          xAxis={[{ scaleType: "point", data: hourlyCheckAt }]}
           sx={{
             [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
               fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
@@ -76,4 +130,7 @@ function Day() {
 
 export default Day;
 
-const DashboardContents = styled.div``;
+const DashboardContents = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
--- allclear/src/components/period/Week.jsx
+++ allclear/src/components/period/Week.jsx
@@ -1,28 +1,22 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { getDailyData } from "../../apis/statistic/statisticData";
 import { axisClasses } from "@mui/x-charts/ChartsAxis";
 
 function Week() {
-  const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-  const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-  const xLabels = [
-    "Page A",
-    "Page B",
-    "Page C",
-    "Page D",
-    "Page E",
-    "Page F",
-    "Page G",
-  ];
-  const [dailyData, setDailyData] = useState({});
+  const [dailyTemperature, setDailyTemperature] = useState([]);
+  const [dailyHumidity, setDailyHumidity] = useState([]);
+  const [dailyLight, setDailyLight] = useState([]);
+  const [dailyCheckAt, setDailyCheckAt] = useState([]);
 
   useEffect(() => {
     getDailyData(
       ({ data }) => {
-        setDailyData(data);
-        console.log(dailyData);
+        setDailyTemperature(data.temperatureList);
+        setDailyHumidity(data.humidityList);
+        setDailyLight(data.lightList);
+        setDailyCheckAt(data.checkAtList);
       },
       (error) => {
         console.log(error);
@@ -36,17 +30,77 @@ function Week() {
         <LineChart
           width={700}
           height={400}
+          colors={["#E03F69"]}
           series={[
-            { data: pData, label: "pv" },
-            { data: uData, label: "uv" },
+            { data: dailyTemperature, label: "온도" },
+            // { data: dailyHumidity, label: "습도" },
+            // { data: dailyLight, label: "조도" },
           ]}
-          xAxis={[{ scaleType: "point", data: xLabels }]}
-          // series={[
-          //   { data: dailyData.temperature, label: "온도" },
-          //   { data: dailyData.humidity, label: "습도" },
-          //   { data: dailyData.light, label: "조도" },
-          // ]}
-          // xAxis={[{ scaleType: "point", data: dailyData.checkAt }]}
+          xAxis={[{ scaleType: "point", data: dailyCheckAt }]}
+          sx={{
+            [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 축 라벨의 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // x축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // y축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // x축 눈금의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // y축 눈금의 색상을 흰색으로 설정
+            },
+          }}
+          slotProps={{ legend: { labelStyle: { fill: `#e6e5ea` } } }}
+        />
+        <LineChart
+          width={700}
+          height={400}
+          colors={["#4A5ED8"]}
+          series={[
+            // { data: dailyTemperature, label: "온도" },
+            { data: dailyHumidity, label: "습도" },
+            // { data: dailyLight, label: "조도" },
+          ]}
+          xAxis={[{ scaleType: "point", data: dailyCheckAt }]}
+          sx={{
+            [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tickLabel}`]: {
+              fill: "#e6e5ea", // 축 라벨의 텍스트 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // x축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.line}`]: {
+              stroke: "#e6e5ea", // y축 선의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionX} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // x축 눈금의 색상을 흰색으로 설정
+            },
+            [`& .${axisClasses.directionY} .${axisClasses.tick}`]: {
+              stroke: "#e6e5ea", // y축 눈금의 색상을 흰색으로 설정
+            },
+          }}
+          slotProps={{ legend: { labelStyle: { fill: `#e6e5ea` } } }}
+        />
+        <LineChart
+          width={700}
+          height={400}
+          colors={["#F6C863"]}
+          series={[
+            // { data: dailyTemperature, label: "온도" },
+            // { data: dailyHumidity, label: "습도" },
+            { data: dailyLight, label: "조도" },
+          ]}
+          xAxis={[{ scaleType: "point", data: dailyCheckAt }]}
           sx={{
             [`& .${axisClasses.directionX} .${axisClasses.tickLabel}`]: {
               fill: "#e6e5ea", // 텍스트 색상을 흰색으로 설정
@@ -76,4 +130,7 @@ function Week() {
 
 export default Week;
 
-const DashboardContents = styled.div``;
+const DashboardContents = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
