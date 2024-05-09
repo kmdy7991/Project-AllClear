739a103db667ac770636ab329b4a6c2ad87bae1f
박성인 <psi7218@naver.com>
2024-05-09 17:54:09 +0900

    [Add] websocket 소스코드 작성

--- allclear/src/components/Monitoring.jsx
+++ allclear/src/components/Monitoring.jsx
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useCallback, useRef, useState, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Monitoring() {
-  const { unityProvider, isLoaded } = useUnityContext({
+  const { unityProvider, sendMessage } = useUnityContext({
     loaderUrl: "Build/Downloads.loader.js",
     dataUrl: "Build/Downloads.data",
     frameworkUrl: "Build/Downloads.framework.js",
     codeUrl: "Build/Downloads.wasm",
   });
 
+  const ws = useRef(null);
+  const [wsData, setWsData] = useState(null);
+
+  useEffect(() => {
+    let wsUrl = "wss://localhost:8000/"; // 여기에 웹소켓 url 넣고
+    ws.current = new WebSocket(wsUrl);
+
+    // Websocket이 연결됐을때, 호출되는 함수
+    ws.current.onopen = () => {
+      console.log("Websocket 연결");
+    };
+
+    //Websocket으로 부터 데이터를 받을 때, 호출되는 함수
+    ws.current.onmessage = (event) => {
+      console.log("데이터 전달 받음", event.data);
+      setWsData(event.data);
+    };
+
+    // 오류 발생 시, 호출되는 함수
+    ws.current.onerror = (error) => {
+      console.log("error :", error);
+    };
+
+    // 연결 해제시 호출되는 함수
+    ws.current.onclose = () => {
+      console.log("Websokcet 연결 해제");
+    };
+  }, []);
+  const testfunction = () => {
+    const activationData = JSON.stringify({
+      isActive: true,
+      isHarvesting: true,
+    });
+    console.log("Sending message to Unity:", activationData);
+    sendMessage("Machine", "ActivateHarvesting", activationData);
+  };
   return (
     <>
       <p>시뮬 화면</p>
+      <button onClick={testfunction}>테스트 버튼</button>
       <Unity
         style={{
           width: "90%",
