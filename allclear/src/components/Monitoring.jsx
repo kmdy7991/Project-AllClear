74fd158ea379b329d9e5ef1f56b617fb43d553e1
박성인 <psi7218@naver.com>
2024-05-12 16:30:46 +0900

    [Add] back과 연결 성공

--- allclear/src/components/Monitoring.jsx
+++ allclear/src/components/Monitoring.jsx
@@ -11,28 +11,54 @@ function Monitoring() {
 
   const ws = useRef(null);
   const [wsData, setWsData] = useState(null);
+  const [activeData, setActiveData] = useState(null);
+  const [alarmData, setAlarmData] = useState(null);
 
   useEffect(() => {
     fetchSSE();
   }, []);
 
+  useEffect(() => {
+    if (activeData) {
+      const activationData = JSON.stringify(activeData);
+      console.log("Sending message to Unity:", activationData);
+      sendMessage("Machine", "ActivateHarvesting", activationData);
+    }
+  }, [activeData]);
+
+  useEffect(() => {
+    if (alarmData) {
+      const alarm = JSON.stringify(alarmData);
+      sendMessage("FireAlarm", "ActivateAlarm", alarm);
+    }
+  }, [alarmData]);
+
   const fetchSSE = async () => {
     const eventSource = new EventSource(
-      "http://192.168.31.213:8081/api/unity/connect"
+      // "http://192.168.31.213:8081/api/unity/connect"
+      "http://192.168.219.92:8081/api/unity/connect"
     );
     eventSource.onopen = () => {
       console.log("sse OPENED");
     };
 
+    // 수확
     eventSource.addEventListener("secondmessage", (e) => {
       console.log(e.data);
+      setActiveData(JSON.parse(e.data));
+    });
+
+    // 경보 알람
+    eventSource.addEventListener("fireAlarm", (e) => {
+      console.log(e.data);
+      setAlarmData(JSON.parse(e.data));
     });
   };
 
   const testfunction = () => {
     const activationData = JSON.stringify({
-      isActive: true,
-      isHarvesting: true,
+      doActive: true,
+      doHarvesting: true,
     });
     console.log("Sending message to Unity:", activationData);
     sendMessage("Machine", "ActivateHarvesting", activationData);
@@ -41,16 +67,22 @@ function Monitoring() {
   return (
     <>
       <p>시뮬 화면</p>
-      <button onClick={testfunction}>테스트 버튼</button>
-      <Unity
+      <div
         style={{
-          width: "90%",
-          height: "80%",
+          display: "flex",
           justifyContent: "center",
-          alignSelf: "center",
+          alignItems: "center",
+          height: "100vh", // 전체 뷰포트 높이
         }}
-        unityProvider={unityProvider}
-      />
+      >
+        <Unity
+          style={{
+            width: "70%", // 너비 70%
+            height: "70%", // 높이 70%
+          }}
+          unityProvider={unityProvider}
+        />
+      </div>
     </>
   );
 }
