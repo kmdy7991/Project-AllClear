fb56ab8da581496c1f53b43ca32c58a3d64e5262
kmdy7991 <kmdy7991@naver.com>
2024-05-20 06:11:14 +0900

    fix: dockerfile

--- websocket/main.py
+++ websocket/main.py
@@ -10,10 +10,10 @@ from py_eureka_client import eureka_client
 @asynccontextmanager
 async def start_load(init: FastAPI):
     # schedule.start()
-    await eureka_client.init_async(eureka_server="http://192.168.161.182:3020/eureka",
+    await eureka_client.init_async(eureka_server="http://k10b302.p.ssafy.io:30200/eureka",
                                    app_name="websocket-service",
                                    instance_ip="127.0.0.1",
-                                   instance_port=3026
+                                   instance_port=3023
                                    )
     yield
 
@@ -27,4 +27,4 @@ async def startup_event():
 app.include_router(websocket_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=3026)
+    uvicorn.run(app, host="0.0.0.0", port=3023)
