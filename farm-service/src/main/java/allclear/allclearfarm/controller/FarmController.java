7cc1bd7ac2d574d6594359c3c5623cc8ae00f955
kmdy7991 <kmdy7991@naver.com>
2024-05-20 05:55:58 +0900

    fix: yaml update

--- farm-service/src/main/java/allclear/allclearfarm/controller/FarmController.java
+++ farm-service/src/main/java/allclear/allclearfarm/controller/FarmController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping(value = "/api/farm-service")
 @RequiredArgsConstructor
 public class FarmController {
   private final FarmService farmService;
