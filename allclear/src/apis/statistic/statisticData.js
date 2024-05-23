bf4ba608487a95e9839e4cf8cc8c6db57fc4a6f1
kmdy7991 <kmdy7991@naver.com>
2024-05-23 14:15:06 +0900

    fix: back path update

--- allclear/src/apis/statistic/statisticData.js
+++ allclear/src/apis/statistic/statisticData.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 const getHourlyData = async (success, fail) => {
   await axios
-    .get(`http://k10b302.p.ssafy.io:30204/hourly`)
+    .get(`http://k10b302.p.ssafy.io:30201/api/state-service/hourly`)
     .then(success)
     .catch(fail);
 };
 
 const getDailyData = async (success, fail) => {
   await axios
-    .get(`http://k10b302.p.ssafy.io:30204/daily`)
+    .get(`http://k10b302.p.ssafy.io:30201/api/state-service/daily`)
     .then(success)
     .catch(fail);
 };
 
 const getLineData = async (lineNumber, success, fail) => {
   await axios
-    .get(`http://k10b302.p.ssafy.io:30207/line/${lineNumber}`)
+    .get(`http://k10b302.p.ssafy.io:30201/api/line-service/line/${lineNumber}`)
     .then(success)
     .catch(fail);
 };
