f982b2413b2686ac7590e2136c93bcd9113414cd
kmdy7991 <kmdy7991@naver.com>
2024-05-27 15:23:14 +0900

    feat: argo test2

--- allclear-gateway/src/main/java/test/TestController.java
+++ allclear-gateway/src/main/java/test/TestController.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
-   @GetMapping("/test")
-   public String test(){
-       return @Value("spring.cloud.gatewayroutes") String s;
-   }
-}
+// @RestController
+// public class TestController {
+//    @GetMapping("/test")
+//    public String test(){
+//        return @Value("spring.cloud.gatewayroutes") String s;
+//    }
+// }
